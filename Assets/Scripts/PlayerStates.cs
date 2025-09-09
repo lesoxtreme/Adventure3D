7daa08c02f917ccfd2c8d707f94a3e3601ebf773
lesoxtreme <38412180+lesoxtreme@users.noreply.github.com>
2025-09-09 06:12:15 -0300

    Feito o script de estados do player.

---
+++ Assets/Scripts/PlayerStates.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Ebac.StateMachine;
+
+public class PlayerStates : MonoBehaviour
+{
+	public enum PlayerStateMachine
+	{
+		WALK,
+		IDLE,
+		JUMP
+	}
+
+	public StateMachine<PlayerStateMachine> stateMachine;
+
+	private void Start()
+	{
+		Init();
+	}
+
+	public void Init()
+	{
+		stateMachine = new StateMachine<PlayerStateMachine>();
+		stateMachine.Init();
+		stateMachine.RegisterStates(PlayerStateMachine.IDLE, new StateBase());
+		stateMachine.RegisterStates(PlayerStateMachine.JUMP, new StateBase());
+		stateMachine.RegisterStates(PlayerStateMachine.WALK, new StateBase());
+
+		stateMachine.SwitchState(PlayerStateMachine.IDLE);
+	}
+}
