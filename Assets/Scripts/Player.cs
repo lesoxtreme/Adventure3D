5b465d2bcf77d954ef40ec8b3757ea3efd84da3f
lesoxtreme <38412180+lesoxtreme@users.noreply.github.com>
2025-09-10 15:56:26 -0300

    Adicionado Movimentação do Player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[Header("Run Setup")]
+	public KeyCode keyRun = KeyCode.LeftShift;
+	public float speedRun = 1.5f;
+
+	public CharacterController characterController;
+	public float speed = 1f;
+	public float turnSpeed = 1f;
+	public float gravity = 9.8f;
+	private float vSpeed = 0f;
+	public Animator animator;
+	public float jumpSpeed = 15f;
+	void Update()
+	{
+			transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+			var inputAxisVertical = Input.GetAxis("Vertical");
+			var speedVector = transform.forward * inputAxisVertical * speed;
+
+			if(characterController.isGrounded)
+			{
+				vSpeed = 0;
+				if(Input.GetKeyDown(KeyCode.Space))
+				{
+					vSpeed = jumpSpeed;
+				}
+			}
+			vSpeed -= gravity * Time.deltaTime;
+			speedVector.y = vSpeed;
+			var isWalking = inputAxisVertical != 0;
+			if(isWalking)
+			{
+				if(Input.GetKey(keyRun))
+				{
+					speedVector *= speedRun;
+					animator.speed = speedRun;
+				}
+				else
+				{
+					animator.speed = 1;
+				}
+			}
+			characterController.Move(speedVector * Time.deltaTime);
+
+			animator.SetBool("Run", inputAxisVertical !=0);
+
+			
+
+	}
+}
